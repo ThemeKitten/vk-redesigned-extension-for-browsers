ff9854005cb7ad8fcdf36a19eeea37203cc5b74a
angelsmedia <angelsmedia@yandex.ru>
2014-08-25 08:52:26 +0300

    delete jquery

--- nnjffehnhfajhilmapidnekkaoofonlh/1.0_0/javascript.js
+++ nnjffehnhfajhilmapidnekkaoofonlh/1.0_0/javascript.js
@@ -1,13 +1,1 @@
-$('body').addClass('redesigned');
-
-$(function(){
-    $(window).scroll(function(){
-        if ($(window).scrollTop()>$('.narrow_column').height()+110) {
-            $('.post').addClass('scroll');
-            $('#content .wide_column').addClass('scroll');
-        } else {
-            $('.post').removeClass('scroll');
-            $('#content .wide_column').removeClass('scroll');
-        };
-    });
-});
+document.getElementsByTagName('body')[0].className+='redesigned';
